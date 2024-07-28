1c790f7dc0e95eccec495657d5de2c6427097dc9
Marta Prystatska <tradlisboa@gmail.com>
2024-07-28 20:32:56 +0100

    add FAQ component

--- client/src/components/PriceComponents/FAQ/index.jsx
+++ client/src/components/PriceComponents/FAQ/index.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import faqData from './data/faqData.json'; 
-import styles from './FAQ.module.sass'
+import React, { useState } from 'react';
+import faqData from './data/faqData.json';
+import styles from './FAQ.module.sass';
 
-const FAQ = () => (
-  <section className={styles.faqs}>
-    {faqData.map((faq, index) => (
-      <div key={index} className={styles.faq}>
-        <h2>{faq.question}</h2>
-        <p>{faq.answer}</p>
-      </div>
-    ))}
-  </section>
-);
+const FAQ = () => {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  // Função para alternar o índice do FAQ aberto
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  // Renderiza cada FAQ
+  const faqs = faqData.faqs.map((faq, index) => {
+    const isOpen = openIndex === index;
+
+    return (
+      <section
+        key={index}
+        className={`${styles.faq} ${isOpen ? styles.open : ''}`}
+      >
+        <h3>
+          {faq.question}
+          <button
+            onClick={() => handleToggle(index)}
+            className={styles.toggleButton}
+          >
+            {isOpen ? 'x' : '+'}
+          </button>
+        </h3>
+        {isOpen && <p>{faq.answer}</p>}
+      </section>
+    );
+  });
+
+  return (
+    <section className={styles.faqs}>
+      <h2>{faqData.titleFaq}</h2>
+      {faqs}
+    </section>
+  );
+};
 
 export default FAQ;
